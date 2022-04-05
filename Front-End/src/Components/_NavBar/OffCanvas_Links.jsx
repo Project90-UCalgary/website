d427ff97f4f04e0f3e8f14e51d6aaf4f88dcc9d5
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-04-05 00:54:08 -0400

    Add Text to Links

--- Front-End/src/Components/_NavBar/OffCanvas_Links.jsx
+++ Front-End/src/Components/_NavBar/OffCanvas_Links.jsx
@@ -2,25 +2,31 @@
 import { Fragment } from "react";
 
 // Style
+import "../Style/_NavBar/OffCanvas_Links.scss";
 
 export default function Links(props) {
   return (
     <Fragment>
       <ul className="links-list">
-        <li className="home-link">
+        <li className="link">
           <ion-icon name="home-outline"></ion-icon>
+          <p className="item-text">Home</p>
         </li>
-        <li className="gallery-link">
+        <li className="link">
           <ion-icon name="images-outline"></ion-icon>
+          <p className="item-text">Gallery</p>
         </li>
-        <li className="projects-link">
+        <li className="link">
           <ion-icon name="briefcase-outline"></ion-icon>
+          <p className="item-text">Projects</p>
         </li>
-        <li className="members-link">
+        <li className="link">
           <ion-icon name="person-outline"></ion-icon>
+          <p className="item-text">Members</p>
         </li>
-        <li className="about-link">
+        <li className="link">
           <ion-icon name="information-circle-outline"></ion-icon>
+          <p className="item-text">About Us</p>
         </li>
       </ul>
     </Fragment>
