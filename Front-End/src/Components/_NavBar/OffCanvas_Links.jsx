828e11dc5399372235434f7755fd36aaaf4aa397
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-04-05 01:19:52 -0400

    Use React Links

--- Front-End/src/Components/_NavBar/OffCanvas_Links.jsx
+++ Front-End/src/Components/_NavBar/OffCanvas_Links.jsx
@@ -1,5 +1,6 @@
 // Libraries
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 // Style
 import "../Style/_NavBar/OffCanvas_Links.scss";
@@ -8,26 +9,36 @@ export default function Links(props) {
   return (
     <Fragment>
       <ul className="links-list">
-        <li className="link">
-          <ion-icon name="home-outline"></ion-icon>
-          <p className="item-text">Home</p>
-        </li>
-        <li className="link">
-          <ion-icon name="images-outline"></ion-icon>
-          <p className="item-text">Gallery</p>
-        </li>
-        <li className="link">
-          <ion-icon name="briefcase-outline"></ion-icon>
-          <p className="item-text">Projects</p>
-        </li>
-        <li className="link">
-          <ion-icon name="person-outline"></ion-icon>
-          <p className="item-text">Members</p>
-        </li>
-        <li className="link">
-          <ion-icon name="information-circle-outline"></ion-icon>
-          <p className="item-text">About Us</p>
-        </li>
+        <Link to="/" className="link-component">
+          <li className="link">
+            <ion-icon name="home-outline"></ion-icon>
+            <p className="item-text">Home</p>
+          </li>
+        </Link>
+        <Link to="/gallery" className="link-component">
+          <li className="link">
+            <ion-icon name="images-outline"></ion-icon>
+            <p className="item-text">Gallery</p>
+          </li>
+        </Link>
+        <Link to="/projects" className="link-component">
+          <li className="link">
+            <ion-icon name="briefcase-outline"></ion-icon>
+            <p className="item-text">Projects</p>
+          </li>
+        </Link>
+        <Link to="/members" className="link-component">
+          <li className="link">
+            <ion-icon name="person-outline"></ion-icon>
+            <p className="item-text">Members</p>
+          </li>
+        </Link>
+        <Link to="/about" className="link-component">
+          <li className="link">
+            <ion-icon name="information-circle-outline"></ion-icon>
+            <p className="item-text">About Us</p>
+          </li>
+        </Link>
       </ul>
     </Fragment>
   );
