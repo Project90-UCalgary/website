fe5fedef597899a8f685e579b03a17172498f32f
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-05-16 17:22:36 -0400

    Fix Moto style and text

--- Front-End/src/Components/_Homepage/Moto.jsx
+++ Front-End/src/Components/_Homepage/Moto.jsx
@@ -10,8 +10,14 @@ export default function Moto(props) {
       <div className="moto-container">
         <button className="join-us-btn">Join Us!</button>
         <p className="moto-text">
-          Most of the benefits of today's engineering are felt by just 10% of
-          the population. Project90 engineers are for the other 90%
+          Project 90 is a club that aims to provide engineering resources to
+          communities that need it the most. We use values of volunteerism and
+          apply technical knowledge to make a worldwide impact. In the words of
+          Project90’s second president, John Ip, “We learned that 90% of the
+          engineering resources in the world benefits 10% of the world, hence
+          our slogan from the beginning was ‘Engineering for the other 90%’."
+          We’re thrilled to be a diverse student group representing a wide
+          variety of nationalities, faiths, and degree majors.
         </p>
       </div>
     </Fragment>
