a8bd9dddc9ce77d24ec780592c80998306ea3eab
Nicolas Alkhoury <nicolaskhoury98@gmail.com>
2022-05-21 05:33:39 -0400

    Test Mock Data

--- Back-End/routes/index.js
+++ Back-End/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
+// Mock Data
+const home = {
+  moto: "askljdnvakljsfnvlksjerv",
+  picture: "https://i.imgur.com/5mNOpit.png"
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.send(home);
 });
 
 module.exports = router;
